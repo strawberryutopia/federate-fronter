b34586d600144e80cd695556005de414a2af9bfb
Lucy Davinhart <lucy@lucymhdavies.com>
2021-06-12 14:20:25 +0100

    Copied name logic over for avatars

--- slack_members.go
+++ slack_members.go
@@ -1,6 +1,7 @@
 package federate
 
 import (
+	"encoding/json"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -17,71 +18,52 @@ type SlackMember struct {
 	} `mapstructure:"workspace_overrides,omitempty"`
 }
 
+func (m SlackMember) ToString() string {
+
+	json, _ := json.Marshal(m)
+
+	return string(json)
+}
+
 type SlackMembers map[string]SlackMember
 
 // TODO: These functions need tests!
 
-func (sm SlackMembers) GetName(fronter, workspace string) (name string) {
+func (sm SlackMembers) GetName(fronter, workspace string) string {
 	log.Debugf("Getting name for %s in %s", fronter, workspace)
 
 	// Use lcase fronter as a key for our SlackMembers config
 	fronterLcase := strings.ToLower(fronter)
 
-	// Do we have specific member config for this fronter?
-	if config, exists := sm[fronterLcase]; exists {
-		log.Debugf("Fronter Config: %v", config)
+	for _, fronterOrDefault := range []string{fronterLcase, "_default"} {
+		// Do we have specific member config for this fronter?
+		if config, exists := sm[fronterOrDefault]; exists {
+			log.Debugf("Fronter %s Config: %v", fronterOrDefault, config.ToString())
 
-		// Do we have specific workspace config for this fronter?
-		workspaceConfig, wsConfigExists := config.WorkspaceOverrides[workspace]
-		if wsConfigExists {
-			if workspaceConfig.Name != nil {
-				log.Debugf("Fronter name defined for %s: %s",
-					workspace,
-					*workspaceConfig.Name)
+			// Do we have specific workspace config for this fronter?
+			workspaceConfig, wsConfigExists := config.WorkspaceOverrides[workspace]
+			if wsConfigExists {
+				if workspaceConfig.Name != nil {
+					log.Debugf("Fronter name defined for %s: %s",
+						workspace,
+						*workspaceConfig.Name)
 
-				return *workspaceConfig.Name
+					return *workspaceConfig.Name
+				}
+
+				// So Workspace config exists, but does not specify a name
+				// Fallback then to Member config
 			}
 
-			// So Workspace config exists, but does not specify a name
-			// Fallback then to Member config
-		}
-
-		// Do we have a name defined at the Member level?
-		if config.Name != nil {
-			log.Debugf("Fronter name defined: %s", *config.Name)
-			return *config.Name
-		}
-
-	}
-
-	log.Debugf("No name configured for %s in %s", fronterLcase, workspace)
-
-	if config, exists := sm["_default"]; exists {
-		log.Debugf("Default Fronter Config: %v", config)
-
-		// Do we have specific workspace config for this fronter?
-		workspaceConfig, wsConfigExists := config.WorkspaceOverrides[workspace]
-		if wsConfigExists {
-			if workspaceConfig.Name != nil {
-				log.Debugf("Fronter name defined for %s: %s",
-					workspace,
-					*workspaceConfig.Name)
-
-				return *workspaceConfig.Name
+			// Do we have a name defined at the Member level?
+			if config.Name != nil {
+				log.Debugf("Fronter name defined: %s", *config.Name)
+				return *config.Name
 			}
 
-			// So Workspace config exists, but does not specify a name
-			// Fallback then to Member config
-		}
-
-		// Do we have a name defined at the Member level?
-		if config.Name != nil {
-			log.Debugf("Fronter name defined: %s", *config.Name)
-			return *config.Name
+			log.Debugf("No name configured for %s in %s", fronterOrDefault, workspace)
 		}
 	}
-
-	log.Debugf("No defaults configured")
 
 	// If we've got to this point, and we still haven't got a name
 	// use the one specified when we called the function
@@ -89,6 +71,42 @@ func (sm SlackMembers) GetName(fronter, workspace string) (name string) {
 }
 
 func (sm SlackMembers) GetAvatar(fronter, workspace, api_avatar string) string {
+	log.Debugf("Getting avatar for %s in %s", fronter, workspace)
 
+	// Use lcase fronter as a key for our SlackMembers config
+	fronterLcase := strings.ToLower(fronter)
+
+	for _, fronterOrDefault := range []string{fronterLcase, "_default"} {
+		// Do we have specific member config for this fronter?
+		if config, exists := sm[fronterOrDefault]; exists {
+			log.Debugf("Fronter %s Config: %v", fronterOrDefault, config.ToString())
+
+			// Do we have specific workspace config for this fronter?
+			workspaceConfig, wsConfigExists := config.WorkspaceOverrides[workspace]
+			if wsConfigExists {
+				if workspaceConfig.Avatar != nil {
+					log.Debugf("Fronter avatar defined for %s: %s",
+						workspace,
+						*workspaceConfig.Avatar)
+
+					return *workspaceConfig.Avatar
+				}
+
+				// So Workspace config exists, but does not specify an avatar
+				// Fallback then to Member config
+			}
+
+			// Do we have an avatar defined at the Member level?
+			if config.Avatar != nil {
+				log.Debugf("Fronter avatar defined: %s", *config.Avatar)
+				return *config.Avatar
+			}
+
+			log.Debugf("No avatar configured for %s in %s", fronterOrDefault, workspace)
+		}
+	}
+
+	// If we've got to this point, and we still haven't got an avatar
+	// use the one specified when we called the function
 	return api_avatar
 }
--- strawbcli/test.sh
+++ strawbcli/test.sh
@@ -4,30 +4,33 @@ echo ====================
 echo Testing...
 echo ====================
 
+# TODO: This is okay for now, but really this aught to be some
+# proper Go tests, with mocks and stuff
+
 
 echo "Lucy"
-go run main.go federate --name Lucy --avatar "https://pbs.twimg.com/media/Ec6Zv21WsAAIwOS?format=png&name=medium"
+go run main.go -d federate --name Lucy --avatar "https://pbs.twimg.com/media/Ec6Zv21WsAAIwOS?format=png&name=medium"
 go run main.go federate check strawb | jq .real_name
 go run main.go federate check pcw | jq .real_name
 
 echo
 echo
 echo "Ivie"
-go run main.go federate --name Ivie --avatar "https://ca.slack-edge.com/T04FFM9U7-U4UBY7Y5V-02da57a94a10-512"
+go run main.go -d federate --name Ivie --avatar "https://ca.slack-edge.com/T04FFM9U7-U4UBY7Y5V-02da57a94a10-512"
 go run main.go federate check strawb | jq .real_name
 go run main.go federate check pcw | jq .real_name
 
 echo
 echo
 echo "Jesper"
-go run main.go federate --name Jesper --avatar "https://i.imgur.com/4vlRY23.png"
+go run main.go -d federate --name Jesper --avatar "https://i.imgur.com/4vlRY23.png"
 go run main.go federate check strawb | jq .real_name
 go run main.go federate check pcw | jq .real_name
 
 echo
 echo
 echo "No Config Set"
-go run main.go federate --name "No Config Set" --avatar "https://www.pngitem.com/pimgs/m/575-5759580_anonymous-avatar-image-png-transparent-png.png"
+go run main.go -d federate --name "No Config Set" --avatar "https://www.pngitem.com/pimgs/m/575-5759580_anonymous-avatar-image-png-transparent-png.png"
 go run main.go federate check strawb | jq .real_name
 go run main.go federate check pcw | jq .real_name
 
